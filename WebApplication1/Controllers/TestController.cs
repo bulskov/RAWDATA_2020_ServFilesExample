f27ea7715fee853a6bea078e450055616e5d98c7
Henrik Bulskov <bulskov@ruc.dk>
2020-11-17 11:40:03 +0100

    Add project files.

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/tests")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+        }
+    }
+}
